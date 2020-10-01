8116284ecda7e797682749eb1f96f9c5b2f6d854
Edgar Lara <65983215+Edgar-La@users.noreply.github.com>
2020-10-01 15:36:59 -0500

    C code added

--- Display8_Delay/main.c
+++ Display8_Delay/main.c
@@ -1,15 +1,14 @@
 #include <msp430.h>
 
-void delay(int time_);
+void delay(int time_);      //Prototype delay function
 
 int main(void)
 {
 	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
 	P2DIR |=0xFF;   //Defino los pines de salida (para display 8 segmentos)
-	P1REN = BIT3;   //Desabilitar resistencia pull-up para push button
-	P2OUT = 0x3F;   //Inicializo en cero
+
 	int i=0;
-int	OUTPUTS[10] = {0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x7F, 0x6F, 0x3F};
+	int	OUTPUTS[10] = {0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x7F, 0x6F, 0x3F};
 	
     for(;;) //Infinite loop
     {
