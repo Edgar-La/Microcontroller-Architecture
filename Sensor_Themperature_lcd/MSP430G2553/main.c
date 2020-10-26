e860d1ef118bf5d6e233e110edc0cafe208f2815
Edgar Lara <65983215+Edgar-La@users.noreply.github.com>
2020-10-25 22:55:56 -0600

    C code updated

--- Sensor_Themperature_lcd/MSP430G2553/main.c
+++ Sensor_Themperature_lcd/MSP430G2553/main.c
@@ -1,77 +1,45 @@
-//Librerias
-#include <msp430.h> 
-#include "lcdLib.h"
-#include <stdio.h>
+#include <msp430.h>
+#include "intrinsics.h"
+#include "lcdLib.h"                                     //Se modifico para que tenga salidas en Puerto 3
 
-
-volatile long valFromADC = 0;
-volatile int valTemp;
-
-void ADC10Init()
+int main( void )
 {
-    //Confiuro el ADC10 para lectura del sensor de temperatura
-    ADC10CTL0 = SREF_1 + ADC10SHT_3 + REFON + ADC10ON;// + ADC10IE;
-    ADC10CTL1 = INCH_3 + SHS_0 + ADC10DIV_2 + ADC10SSEL_0 + CONSEQ_0;
-    //ADC10CTL1 = INCH_3 + ADC10DIV_2 + ADC10SSEL_0 + CONSEQ_0;
-    ADC10AE0 |= BIT3;
+   WDTCTL = WDTPW + WDTHOLD;                            // Stop watchdog timer to prevent time out reset
+
+   ADC10CTL0 = SREF_2 + REFBURST + ADC10ON; //Las eqtiquetas indican lo que hace
+   ADC10AE0 |= BIT0+BIT4;                               //Indico los pines que voy a samplear
+   P1DIR |= 0b11100000;                                     //Indico la salida de LEDs de temperatura
+
+   int Temp_num;
+   char Temp[16];
+
+
+   //Inicio la pantalla lcd
+   lcdInit();
+   lcdSetText("Inicializando...", 0, 0);
+
+   for (;;)
+   {
+      ADC10CTL0 |= ENC + ADC10SC;             //Activo e inicio la conversión
+
+
+      //Temp_num = ADC10MEM*(150/1023);
+      //Temp_num = ADC10MEM*(0.146627566);
+      Temp_num = (ADC10MEM*0.2326)-87.33;
+      sprintf(Temp, "%d", Temp_num);
+      strcat(Temp, "*C ");
+      lcdSetText("Temperatura de:", 0, 0);
+      lcdSetText(Temp, 0, 1);
+
+
+      if (Temp_num > 75)
+          P1OUT = 0b00100000;                        //Temp. Alta, LED rojo ON
+      else if (Temp_num >= 25 && Temp_num <= 75)
+          P1OUT = 0b01000000;                        //Temp. Media, LED verde ON
+      else if (Temp_num < 25)
+          P1OUT = 0b10000000;                        //Temp. Baja, LED azul ON
+      else
+          P1OUT &= ~0b11100000; ;                     //Si o si, todos OFF
+   }
 }
 
-void tempInit()
-{
-    ADC10CTL0=SREF_1 + REFON + ADC10ON + ADC10SHT_3 ; //1.5V ref,Ref on,64 clocks for sample
-    ADC10CTL1=INCH_9 + ADC10DIV_3; //temp sensor is at 10 and clock/4
-}
-
-void Edgar()
-{
-    ADC10CTL0 = 0b0100000000010000;          //Uso de referencia externa. Pongo ADC10 On
-    ADC10CTL1 = 0b0000000000000000;          //Como todo. es cero puedo omitir linea, pero es para ser explicito
-    ADC10AE0 |= 0b00010001;                  //Indico que PIN 0 y 1 son analogicos
-                                                 //El PIN 0 es el que voy a samplear
-}
-int main(void)
-{
-	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
-
-	//ADC10Init();
-	//tempInit();
-	Edgar();
-	//P1SEL |= BIT0 + BIT3;
-	//ADC10AE0 |= 0b00010001;
-
-
-	//ADC10CTL0 |= ENC + ADC10SC;      //enable conversion and start conversion
-int Temp_num;
-char Temp[10];
-
-
-//Inicio la pantalla lcd
-	lcdInit();
-	lcdSetText("Temperatura de:", 0, 0);
-
-
-
-    for(;;)
-    {
-        ADC10CTL0 |= ENC + ADC10SC;
-        /*
-        valFromADC = ADC10MEM;
-        valTemp = (valFromADC*1.5)/1023;
-        */
-        valTemp = ADC10MEM;
-
-        //Temp_num = (ADC10MEM/1023)*150;
-        //Temp_num = ADC10MEM * 0.146627566;
-        //Temp_num = tempOut();
-        //Temp_num = ADC10MEM;
-        //Temp_num = ((Temp_num * 27069L - 18169625L)>>16);
-        valTemp = ((valTemp * 27069L - 18169625L)>>16);
-        sprintf(Temp, "%d", valTemp + 5);
-        //gcvt(valTemp, 1, Temp);
-        //if (Temp_num < 100)
-        //lcdClear();
-        //strcat(Temp, "*C");
-        lcdSetText(Temp, 5, 1);
-    }
-
-}
