20629e07118b224b907307408d9bf9d99ebb81c3
Edgar Lara <65983215+Edgar-La@users.noreply.github.com>
2020-12-13 23:14:12 -0600

    Optimized with LEDs

--- ARDUINO/Filters/Test_code.ino
+++ ARDUINO/Filters/Test_code.ino
@@ -4,12 +4,13 @@
 #include <LiquidCrystal_I2C.h>
 
 int LED_C = 13, edo_btns = 0;
+const int ledPH = 9; const int ledPL = 10; const int ledBP = 11; const int ledSB = 12;
 
 char password[] = "1234";
 String pass_check = "";
 //-----------------------Seccion KEYPAD---------------------------------------
 const byte filas = 4; 
-const byte colum = 4;
+const byte colum = 3;
 char teclas[filas][colum] = {
   {'1', '2', '3'},
   {'4', '5', '6'},
@@ -27,23 +28,27 @@ void setup(){
   pinMode(LED_C, OUTPUT);
   digitalWrite(LED_C, !edo_btns);
   Serial.println("Digite '1234'");
-
+  //-----------LEDS
+  pinMode(ledPH , OUTPUT); pinMode(ledPL , OUTPUT); pinMode(ledBP , OUTPUT); pinMode(ledSB , OUTPUT);
+  //---------------
   lcd.begin(16, 2);
   //Cursor en la posicion 3,0
-  lcd.setCursor(3, 0);
+  lcd.setCursor(2, 0);
   lcd.backlight();
-  lcd.print("Contrasena:");
-  //lcd.setCursor(7,1);
-  //lcd.print(":)");
+  lcd.print("Selec Filtro:");
+  lcd.setCursor(0,1);
+  lcd.print("1PH 2PL 3BP 4SB");
+  digitalWrite(ledPH , LOW);
 }
 //-----------------------------------------------------------------------------
 void loop(){
   char customKey = teclado.getKey();
   if (customKey != NO_KEY){
-    digitalWrite(LED_C, HIGH);
-    pass_check += customKey;
-    lcd.clear(); lcd.setCursor(0, 0); lcd.print(pass_check);
-    Serial.println(customKey);
+    //digitalWrite(LED_C, HIGH);
+    //pass_check += customKey;
+    //lcd.clear(); lcd.setCursor(0, 0); lcd.print(pass_check);
+    //Serial.println(customKey);
+    /*
       if (pass_check == password)
       {
         lcd.clear(); lcd.setCursor(3, 0); lcd.print("Contrasena:");
@@ -57,6 +62,28 @@ void loop(){
         
         Serial.println("Testado");
         pass_check = "";
+        }*/
+        switch(customKey){
+          case '1':
+          lcd.clear(); lcd.setCursor(1, 0); lcd.print("1) High Pass:");
+          digitalWrite(ledPH , 1); digitalWrite(ledPL , 0);  digitalWrite(ledBP , 0); digitalWrite(ledSB , 0);
+          Serial.println(customKey);
+          break;
+          case '2':
+          lcd.clear(); lcd.setCursor(2, 0); lcd.print("2) Low Pass:");
+          digitalWrite(ledPH , 0); digitalWrite(ledPL , 1);  digitalWrite(ledBP , 0); digitalWrite(ledSB , 0);
+          Serial.println(customKey);
+          break;
+          case '3':
+          lcd.clear(); lcd.setCursor(2, 0); lcd.print("3) Band Pass:");
+          digitalWrite(ledPH , 0); digitalWrite(ledPL , 0);  digitalWrite(ledBP , 1); digitalWrite(ledSB , 0);
+          Serial.println(customKey);
+          break;
+          case '4':
+          lcd.clear(); lcd.setCursor(2, 0); lcd.print("4) Band Stop:");
+          digitalWrite(ledPH , 0); digitalWrite(ledPL , 0);  digitalWrite(ledBP , 0); digitalWrite(ledSB , 1);
+          Serial.println(customKey);
+          break;
         }
   }
 }
