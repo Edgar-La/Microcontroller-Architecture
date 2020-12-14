f640373eaae41ff7c739c9ffbd0e49d7f5bbc6c4
Edgar Lara <65983215+Edgar-La@users.noreply.github.com>
2020-12-13 22:32:04 -0600

    LCD I2C implemented

--- ARDUINO/Filters/Test_code.ino
+++ ARDUINO/Filters/Test_code.ino
@@ -1,10 +1,13 @@
 #include <Keypad.h>
 #include <string.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
 int LED_C = 13, edo_btns = 0;
 
 char password[] = "1234";
 String pass_check = "";
-
+//-----------------------Seccion KEYPAD---------------------------------------
 const byte filas = 4; 
 const byte colum = 4;
 char teclas[filas][colum] = {
@@ -13,27 +16,38 @@ char teclas[filas][colum] = {
   {'7', '8', '9'},
   {'.', '0', '='}
 };
-
 byte filas_Pines[filas] = {5, 4, 3, 2}; 
 byte colum_Pines[colum] = {8, 7, 6}; 
-
 Keypad teclado = Keypad(makeKeymap(teclas), filas_Pines, colum_Pines, filas, colum); 
-
+//-----------------------Seccion LCD I2C---------------------------------------
+LiquidCrystal_I2C lcd(0x20,16,2);
+//-----------------------------------------------------------------------------
 void setup(){
   Serial.begin(9600);
   pinMode(LED_C, OUTPUT);
   digitalWrite(LED_C, !edo_btns);
   Serial.println("Digite '1234'");
+
+  lcd.begin(16, 2);
+  //Cursor en la posicion 3,0
+  lcd.setCursor(3, 0);
+  lcd.backlight();
+  lcd.print("Contrasena:");
+  //lcd.setCursor(7,1);
+  //lcd.print(":)");
 }
-  
+//-----------------------------------------------------------------------------
 void loop(){
   char customKey = teclado.getKey();
   if (customKey != NO_KEY){
     digitalWrite(LED_C, HIGH);
     pass_check += customKey;
+    lcd.clear(); lcd.setCursor(0, 0); lcd.print(pass_check);
     Serial.println(customKey);
       if (pass_check == password)
       {
+        lcd.clear(); lcd.setCursor(3, 0); lcd.print("Contrasena:");
+        lcd.setCursor(7,1); lcd.print(pass_check);
         digitalWrite(LED_C, edo_btns);
         Serial.println("Iniciando"); delay(500);
         Serial.println(pass_check);
